b34393da91dd5faa4709d64ecf2d265e8fec80f9
sanket karowa <skarowa14@gmail.com>
2022-08-17 00:56:47 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,3 +5,5 @@ API_ID = getenv("API-ID")
 API_HASH = getenv("API-HASH")
 AUTHORIZED_IDS = [1072139158]
 NGROK_URL = ["http://127.0.0.1:4040/api/tunnels"]
+MQTT_HOST = getenv("MQTT_HOST")
+MQTT_PORT = getenv("MQTT_PORT")

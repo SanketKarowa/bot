5461c996a19fceb8b376bd972c7e19943e999305
sanket karowa <skarowa14@gmail.com>
2022-08-17 00:55:24 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,15 +7,25 @@ from pyrogram.errors import PeerIdInvalid
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message, CallbackQuery
 from pyrogram.errors.exceptions import MessageIdInvalid
 from requests import HTTPError
-from config import API_ID, API_HASH, TG_TOKEN, AUTHORIZED_IDS, NGROK_URL
+from config import API_ID, API_HASH, TG_TOKEN, AUTHORIZED_IDS, NGROK_URL, MQTT_HOST, MQTT_PORT
 from logging2 import Logger
 import psutil
+import paho.mqtt.client as mqtt
 
 app = Client("home_ant_bot", api_id=API_ID, api_hash=API_HASH, bot_token=TG_TOKEN)
+mqttc = mqtt.Client()
 logger = Logger(__name__)
 system_info_filter = filters.create(lambda _, __, query: query.data.startswith("sys_info"))
 ngrok_info_filter = filters.create(lambda _, __, query: query.data.startswith("ng_info"))
-
+sol_info_filter = filters.create(lambda _, __, query: query.data.startswith("sol_info"))
+menu_filter = filters.create(lambda _, __, query: query.data.startswith("menu"))
+BATT_TOPIC = "/esp8266/batt"
+RELAY1_TOPIC = "/status/r1"
+RELAY2_TOPIC = "/status/r2"
+MAINS_TOPIC = "/esp8266/main"
+MSG_ID = None
+CHAT_ID = None
+MQTT_DATA = {}
 
 def convert_size(size_bytes) -> str:
     if size_bytes == 0:
@@ -44,6 +54,14 @@ def send_menu(message, chat) -> None:
         app.edit_message_text(chat, message, text="Home-Ant", reply_markup=InlineKeyboardMarkup(buttons))
     except MessageIdInvalid:
         app.send_message(chat, text="Home-Ant", reply_markup=InlineKeyboardMarkup(buttons))
+
+
+@app.on_callback_query(filters=menu_filter)
+def menu(client: Client, callback_query: CallbackQuery) -> None:
+    send_menu(callback_query.from_user.id, callback_query.message.id)
+    if mqttc.is_connected():
+        logger.info(f"disconnecting MQTT server: {MQTT_HOST}:{MQTT_PORT}")
+        mqttc.disconnect()
 
 
 @app.on_message(filters=filters.command("start"))
@@ -124,4 +142,43 @@ def ngrok_info_callback(client: Client, callback_query: CallbackQuery) -> None:
                           parse_mode=enums.ParseMode.HTML,
                           reply_markup=button)
 
+
+def on_connect(client, userdata, flags, rc):
+    logger.info(f"Connected with result code: {str(rc)}")
+    try:
+        client.subscribe([(BATT_TOPIC, 0), (MAINS_TOPIC, 0), (RELAY1_TOPIC, 0), (RELAY2_TOPIC, 0)])
+    except Exception as err:
+        logger.error(f"Error while subscribing: {str(err)}")
+
+
+def on_message(client, userdata, msg):
+    logger.info(f"Topic: {msg.topic} Payload: {str(msg.payload)}")
+    button = InlineKeyboardMarkup([[InlineKeyboardButton("🔙 Menu", "menu")]])
+    global MQTT_DATA
+    MQTT_DATA[msg.topic] = str(msg.payload)
+    text = ""
+    try:
+        for topic in MQTT_DATA:
+            text += f"<b>{topic}</b>: <code>{MQTT_DATA.get(topic)}</code>\n"
+        app.edit_message_text(CHAT_ID, MSG_ID, text, parse_mode=enums.ParseMode.HTML, reply_markup=button)
+    except Exception as e:
+        logger.error(f"error sending topic payload info: {str(e)}")
+
+
+mqttc.on_connect = on_connect
+mqttc.on_message = on_message
+
+
+@app.on_callback_query(filters=sol_info_filter)
+def sol_info_callback(client: Client, callback_query: CallbackQuery) -> None:
+    global MSG_ID
+    global CHAT_ID
+    MSG_ID = callback_query.message.id
+    CHAT_ID = callback_query.from_user.id
+    try:
+        if not mqttc.is_connected():
+            logger.info(f"connecting to MQTT server: {MQTT_HOST}:{MQTT_PORT}")
+            mqttc.connect(host=MQTT_HOST, port=MQTT_PORT)
+    except Exception as err:
+        logger.error(f"error while connecting to MQTT server: {str(err)}")
 
